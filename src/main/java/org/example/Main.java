c0984e09d21818977aa7064ed2a2f5eefd16ecb6
moura1001 <gmouraneto01@gmail.com>
2024-09-16 14:51:24 -0300

    feat: passagem de parâmetros para abrir o relatório 09

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,12 +6,18 @@ import java.sql.SQLException;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        abrirJrxml("02");
+        //abrirJrxml("01");
+        //abrirJrxml("02");
+        abrirJrxml("09");
     }
 
     private static void abrirJrxml(String numero) {
         Connection connection = JdbcConnection.connection();
         JasperService service = new JasperService();
+        if("09".equals(numero)) {
+            service.addParam("NIVEL_DESC", "JUNIOR");
+            service.addParam("UF", "SP");
+        }
         service.abrirJasperViewer("relatorios/jrxml/funcionarios-"+numero+".jrxml", connection);
         try {
             connection.close();
